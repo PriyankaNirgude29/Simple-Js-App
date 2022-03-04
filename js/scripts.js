0bbe30593b12aa2066337590cae89289e95aac2c
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-03-04 21:51:07 +0100

    Updated Scripts.js and Created Array PokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-alert('Hello World !!!');
-let myFavoriteFood = 'Cakes And Cookies';
-document.write(myFavoriteFood);
+let PokemonList = [
+{
+  Name:'Bulbasaur'
+  Height: 0.7,
+  Type: ['grass', 'poison']
+},
+
+{
+  Name:'Butterfree'
+  Height: 1.1,
+  Type: ['bug', 'flying']
+},
+
+{
+  Name:'Caterpie'
+  Height: 0.3,
+  Type: ['bug']
+},
+
+{
+  Name:'Charmeleon'
+  Height: 1.1,
+  Type: ['fire']
+}
+};
